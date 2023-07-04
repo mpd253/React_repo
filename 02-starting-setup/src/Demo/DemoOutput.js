f7d5f53ee7b8d531954e6166f23f0aeb8c5f08cb
ChoiYoungKyu <mpd253@naver.com>
2023-07-04 16:53:32 +0900

    Section 12 finish

---
+++ 02-starting-setup/src/Demo/DemoOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DemoOutput = (props) => {
+  console.log('DemoOutput Running!');
+
+  return (
+    <p>{props.show ? 'This is new!' : ''}</p>
+  );
+};
+
+export default React.memo(DemoOutput);
