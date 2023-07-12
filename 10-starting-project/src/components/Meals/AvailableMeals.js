d6018d8382e2e7279cd12677d547dce5a25e565d
ChoiYoungKyu <mpd253@naver.com>
2023-07-12 19:57:37 +0900

    Section 17 start, 음식 주문 앱에 Http 및 양식 추가

---
+++ 10-starting-project/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const axiosMeals = async () => {
+      const response = await axios.get('https://react-http-66d6a-default-rtdb.firebaseio.com/meals.json');
+      const data = response.data;
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+    axiosMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
